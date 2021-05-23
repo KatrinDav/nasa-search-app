0f4712c757204a9c4a546975f6865a31a72ced09
Kasia Dav <k.dawidowicz2016@gmail.com>
2021-05-23 15:01:42 +0200

    create srtucture of app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './App.module.scss';
+import Header from './components/Header/Header';
+import ListItems from './components/ListItems/ListItems';
+import Title from './components/Title/Title';
+import Subtitle from './components/Subtitle/Subtitle';
+import { motion } from 'framer-motion';
+
+export const MyContext = React.createContext({
+  handleClick: () => { },
+  setQuery: () => { },
+  query: '',
+
+})
+
+const container = {
+  hidden: {
+    opacity: 0,
+    y: 70,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.8,
+      type: 'spring',
+      stiffness: 200
+    },
+  }
+}
 
 function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('');
+
+  const fetchData = async () => {
+    const response = await axios.get(`https://images-api.nasa.gov/search?q=${query}&media_type=image`);
+
+    setData(response.data.collection.items)
+    console.log(response.data.collection.items)
+
+    setQuery('')
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    fetchData();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MyContext.Provider value={{ handleClick, setQuery, query }}>
+      <div className={styles.app}>
+        <Header />
+
+        <motion.div className={styles.wrapper}
+          variants={container}
+          initial='hidden'
+          animate='visible'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Title />
+          <Subtitle />
+        </motion.div>
+        {data && <ListItems data={data} />}
+      </div>
+    </MyContext.Provider>
   );
 }
 
