c0290c8f1b941566780cd5a2f863e6e3b44a9652
Kasia Dav <k.dawidowicz2016@gmail.com>
2021-05-23 15:00:31 +0200

    create Title and Subtitle components with styles

---
+++ src/components/Subtitle/Subtitle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Subtitle.module.scss';
+
+const Subtitle = () => {
+    return (
+        <div className={styles.title_desc}>
+            <h2> Discover image galleries and information about space, stars, planets, galaxies, astronauts and science - all provided by National Aeronautics and Space Administration.</h2>
+        </div>
+
+    );
+}
+
+export default Subtitle;
---
+++ src/components/Title/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Title.module.scss';
+
+
+const Title = () => {
+    return (
+        <>
+            <div className={styles.logo2}>  </div>
+            <h1 className={styles.main_title}>
+                COSMIC <br /> SEARCHER
+        </h1>
+        </>
+    );
+}
+
+export default Title;
